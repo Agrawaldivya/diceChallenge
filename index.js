69b92080886d2b43a601bb5308565fbaa43bcb5e
Agrawaldivya <76201464+Agrawaldivya@users.noreply.github.com>
2023-02-15 21:55:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// Generates random number from 1-6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImages = "dice" + randomNumber1 + ".png";
+
+var img1 = document.querySelectorAll("img")[0];
+
+img1.setAttribute("src", randomDiceImages);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var secondRandomDiceImages = "dice" + randomNumber2 + ".png";
+
+var img2 = document.querySelectorAll("img")[1];
+
+img2.setAttribute("src", secondRandomDiceImages);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
